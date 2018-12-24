6e6f2c255fbb2d5abdab86647f470138b20403ef
laurenalyn <laurenalynwilkerson@gmail.com>
2018-12-23 22:43:53 -0500

    changes structure for counter buttons

--- src/Counters.js
+++ src/Counters.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 const Counters = (props) => {
     return (
-        <div>
-            <button className="button1" onClick={() => { props._LikeMe() }}>Likes: {props.likes}</button>
-            <button className="button2" onClick={() => { props._DislikeMe() }}>Dislikes: {props.unlikes}</button>
+        <div className="counter-btn-container">
+            <div className="button">
+                <a className="button1" onClick={() => { props._LikeMe() }}>Likes: {props.likes}</a>
+            </div>
+            <div className="button">
+                <a className="button2" onClick={() => { props._DislikeMe() }}>Dislikes: {props.unlikes}</a>
+            </div>
         </div>
     )
 }
